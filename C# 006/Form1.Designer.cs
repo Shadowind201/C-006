8dbffe731cce439d6377e2ae316409e90bc88601
Shadowind201 <enzocarnier@gmail.com>
2024-05-09 22:09:59 -0300

    Adicionar arquivos de projeto.

---
+++ C# 006/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace C__006
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBotao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(149, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clique no botão";
+            // 
+            // btnBotao
+            // 
+            this.btnBotao.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.btnBotao.Location = new System.Drawing.Point(175, 140);
+            this.btnBotao.Name = "btnBotao";
+            this.btnBotao.Size = new System.Drawing.Size(113, 47);
+            this.btnBotao.TabIndex = 1;
+            this.btnBotao.Text = "Botão";
+            this.btnBotao.UseVisualStyleBackColor = true;
+            this.btnBotao.Click += new System.EventHandler(this.btnBotao_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 323);
+            this.Controls.Add(this.btnBotao);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBotao;
+    }
+}
+
---
+++ C# 006/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__006
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBotao_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Olá, mundo. Projeto no Visual Studio!");
+            MessageBox.Show("A execução acabou. Bate enter para terminar...");
+            Close();
+        }
+    }
+}
